d08d50076e8f31f8ece026c6e29202d6fc227037
franciSauce <careyfrancisowino@gmail.com>
2025-10-01 09:31:11 +0300

    Add fibs(n)

---
+++ app.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// Fibonacci sequence
+// 1. Iteration
+function fibs(n) {
+    if (n <= 0) return [];
+    if (n === 1) return [0];
+    if (n === 2) return [0, 1];
+
+    const nextNum = [0, 1];
+    for (let i = 2; i < n; i++) {
+        nextNum.push(nextNum[i - 1] + nextNum[i - 2]);
+    }
+    return nextNum;
+}
+
+console.log(fibs(10));
+
+// 2. Recursion
